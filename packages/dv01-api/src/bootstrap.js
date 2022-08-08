74c31e4a5745f4400a5d4a7640782dcce3d9bd46
sjbao <supuilam@gmail.com>
2022-08-08 15:24:53 -0400

    Make API its own MFE

---
+++ packages/dv01-api/src/bootstrap.js
@@ -0,0 +1,12 @@
+import { getData } from './request/api'
+
+if (process.env.NODE_ENV === 'development') {
+    const root = document.getElementById('_dv01-api-dev-root');
+    getData().then(loanData => {
+        root.innerHTML = `<pre>${JSON.stringify(loanData, null, 2)}</pre>`;
+    });
+}
+
+export {
+    getData
+}
---
+++ packages/dv01-api/webpack.config.js
@@ -0,0 +1,29 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+
+module.exports = {
+	mode: 'development',
+	devServer: {
+		port: 8081
+	},
+
+    module: {
+        rules: [
+            {
+                test: /\.csv$/,
+                exclude: /node_modules/,
+                use: [
+                  {
+                    loader: "raw-loader"
+                  }
+                ],
+              }
+        ]
+    },
+
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: './public/index.html'
+		})
+	]
+}
